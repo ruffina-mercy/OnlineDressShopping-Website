0a92909e00023219f7b588fbc6077482ddf504cd
ruffina-mercy <86003290+ruffina-mercy@users.noreply.github.com>
2021-10-09 23:14:17 +0530

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -7,8 +7,8 @@ function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
-        <h1> Pedro's Pizzeria </h1>
-        <p> PIZZA TO FIT ANY TASTE</p>
+        <h1> Fashions </h1>
+        <p> LOOK PRETTY BY WEARING THE FASHIONS COLLECTIONS</p>
         <Link to="/menu">
           <button> ORDER NOW </button>
         </Link>
