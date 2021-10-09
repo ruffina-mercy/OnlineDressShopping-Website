53c95ac5b97d97d34e6a8d38f44c2fbf6c9a8973
ruffina-mercy <86003290+ruffina-mercy@users.noreply.github.com>
2021-10-09 11:36:00 -0600

    Add files via upload

---
+++ Components/Navbar.js
@@ -0,0 +1,31 @@
+import React  from "react";
+import Logo from "../images/dresslog.jpg";
+import { Link } from "react-router-dom";
+
+import "../styles/Navbar.css";
+
+function Navbar() {
+  
+  return (
+    <div className="navbar">
+      <div className="leftSide" >
+        <img src={Logo} alt="Dresses"/>
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+        </div>
+      </div>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Frock from "../images/frock.jpg";
+import Kurta from "../images/kurtaboys.jpg";
+import GirlKids from "../images/kidsdress.jpg";
+import BoyKids from "../images/boykids.jpg";
+import KurtasWomen from "../images/kurtas.jpg";
+import Western from "../images/western.jpg";
+
+export const MenuList = [
+  {
+    name: "Women Frocks",
+    image: Frock,
+    price: 35.99,
+  },
+  {
+    name: "Men Kurta",
+    image: Kurta,
+    price: 25.99,
+  },
+  {
+    name: "Girlkids Dresses",
+    image: GirlKids,
+    price: 15.53,
+  },
+  {
+    name: "Boykids Dresses",
+    image: BoyKids,
+    price: 16.99,
+  },
+  {
+    name: "Women Kurtas",
+    image:KurtasWomen,
+    price: 24.99,
+  },
+  {
+    name: "Western Dresses",
+    image: Western,
+    price: 22.99,
+  },
+];
